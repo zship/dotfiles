a4c2bf8ba761ef811b47b948d2a83aa36379e255
Zach Shipley <zach@zachshipley.com>
2013-09-20 12:02:44 -0500

    Faster and more reliable completion (brew is somewhat slow, apt-get uses bash-completion which is not available until first invocation)

--- home/.bash/p-completion.sh
+++ home/.bash/p-completion.sh
@@ -1,230 +1,85 @@
-cached_function_name() {
-    local fn=$(eval printf "\$P_COMPLETION_FOR_$1" 2>/dev/null)
-    if [ -z "$fn" ]; then
-        fn=$(complete -p "$1" | perl -pe "s/.* -F (.*) $1/\1/")
-        export "P_COMPLETION_FOR_$1"
-        eval "P_COMPLETION_FOR_$1=$fn"
+complete_on_installed() {
+    local pkg_manager="$P_COMPLETION_PKG_MANAGER"
+    local names
+
+    if [ $pkg_manager == 'apt' ]; then
+        names=$(dpkg-query -l "$1*" | grep '^ii' | perl -pe 's/^ii +(\S*) +.*/\1/g')
+    elif [ $pkg_manager == 'brew' ]; then
+        names=$(brew list | grep "^$1")
+    elif [ $pkg_manager == 'yum' ]; then
+        names=$(yum list installed | grep "^$1" | perl -pe 's/^(.*)\s+.*/\1/g')
     fi
-    printf "$fn"
+
+    COMPREPLY=( $names )
 }
 
 
-borrow() {
-    local from="$1"
-    local to="$2"
+complete_on_all() {
+    local pkg_manager="$P_COMPLETION_PKG_MANAGER"
+    local names
 
-    char_difference=$(( $(printf "$to" | wc -m) - $(printf "$from" | wc -m) ))
-    let COMP_POINT+=$char_difference
+    if [ $pkg_manager == 'apt' ]; then
+        names=$(dpkg-query --show --showformat='${Package}\n' "$1*")
+    elif [ $pkg_manager == 'brew' ]; then
+        names=$(find "$(brew --prefix)/Library/Formulas" -name '*.rb' | basename | sed 's/\.rb//g' | grep "^$1")
+    elif [ $pkg_manager == 'yum' ]; then
+        names=$(yum list | grep "^$1" | perl -pe 's/^(.*)\s+.*/\1/g')
+    fi
 
-    word_difference=$(( $(printf "$to" | wc -w) - $(printf "$from" | wc -w) ))
-    let COMP_CWORD+=$word_difference
-
-    COMP_LINE=$( printf "$COMP_LINE" | sed "s/$from/$to/" )
-    COMP_WORDS=( $COMP_LINE )
-
-    local cmd=${COMP_WORDS[0]}
-    local fn=$(cached_function_name "$cmd")
-    "$fn"
+    COMPREPLY=( $names )
 }
 
 
 _p_completion() {
-    local pkg_manager="$P_COMPLETION_PKG_MANAGER"
+    local cur prev words cword
+    _init_completion || return
+
     local command=${COMP_WORDS[1]}
 
-    if [ $pkg_manager == 'apt' ]; then
-
-        case $command in
-            update)
-                borrow 'p update' 'apt-get update'
-                return
-                ;;
-            outdated)
-                borrow 'p outdated' 'apt-get upgrade'
-                return
-                ;;
-            installed)
-                borrow 'p installed' 'dpkg'
-                return
-                ;;
-            files)
-                borrow 'p files' 'dpkg-query -L'
-                return
-                ;;
-            search)
-                borrow 'p search' 'apt-cache search'
-                return
-                ;;
-            info)
-                borrow 'p info' 'apt-cache show'
-                return
-                ;;
-            deplist)
-                borrow 'p deplist' 'apt-cache depends'
-                return
-                ;;
-            install)
-                borrow 'p install' 'apt-get install'
-                return
-                ;;
-            reinstall)
-                borrow 'p reinstall' 'apt-get install --reinstall'
-                return
-                ;;
-            upgrade)
-                borrow 'p upgrade' 'apt-get upgrade'
-                return
-                ;;
-            remove)
-                borrow 'p remove' 'apt-get remove'
-                return
-                ;;
-            purge)
-                borrow 'p purge' 'apt-get remove'
-                return
-                ;;
-            clean)
-                borrow 'p clean' 'apt-get clean'
-                return
-                ;;
-            autoclean)
-                borrow 'p autoclean' 'apt-get autoclean'
-                return
-                ;;
-        esac
-
-    elif [ $pkg_manager == 'brew' ]; then
-
-        case $command in
-            update)
-                borrow 'p update' 'brew update'
-                return
-                ;;
-            outdated)
-                borrow 'p outdated' 'brew outdated'
-                return
-                ;;
-            installed)
-                ;;
-            files)
-                borrow 'p files' 'brew list'
-                return
-                ;;
-            search)
-                borrow 'p search' 'brew search'
-                return
-                ;;
-            info)
-                borrow 'p info' 'brew info'
-                return
-                ;;
-            deplist)
-                borrow 'p deplist' 'brew deps'
-                return
-                ;;
-            install)
-                borrow 'p install' 'brew install'
-                return
-                ;;
-            reinstall)
-                ;;
-            upgrade)
-                borrow 'p upgrade' 'brew upgrade'
-                return
-                ;;
-            remove)
-                borrow 'p remove' 'brew remove'
-                return
-                ;;
-            purge)
-                ;;
-            clean)
-                ;;
-            autoclean)
-                borrow 'p autoclean' 'brew cleanup'
-                return
-                ;;
-        esac
-
-    elif [ $pkg_manager == 'yum' ]; then
-
-        case $command in
-            update)
-                borrow 'p update' 'yum check-update'
-                return
-                ;;
-            outdated)
-                borrow 'p outdated' 'yum check-update'
-                return
-                ;;
-            installed)
-                borrow 'p installed' 'rpm -qa'
-                return
-                ;;
-            files)
-                borrow 'p files' 'rpm -ql'
-                return
-                ;;
-            search)
-                borrow 'p search' 'yum search'
-                return
-                ;;
-            info)
-                borrow 'p info' 'yum info'
-                return
-                ;;
-            deplist)
-                borrow 'p deplist' 'yum deplist'
-                return
-                ;;
-            install)
-                borrow 'p install' 'yum install'
-                return
-                ;;
-            reinstall)
-                borrow 'p reinstall' 'yum reinstall'
-                return
-                ;;
-            upgrade)
-                borrow 'p upgrade' 'yum update'
-                return
-                ;;
-            remove)
-                borrow 'p remove' 'yum erase'
-                return
-                ;;
-            purge)
-                ;;
-            clean)
-                borrow 'p clean' 'yum clean'
-                return
-                ;;
-            autoclean)
-                borrow 'p autoclean' 'yum clean'
-                return
-                ;;
-        esac
-
-    fi
+    case $command in
+        remove|\
+        installed|\
+        files|\
+        upgrade|\
+        purge)
+            complete_on_installed $cur
+            return
+            ;;
+        search|\
+        info|\
+        deplist|\
+        install|\
+        reinstall|\
+        whatprovides)
+            complete_on_all $cur
+            return
+            ;;
+        update|\
+        outdated|\
+        clean|\
+        autoclean)
+            return
+            ;;
+    esac
 
     matching_commands="$(p commands | grep "^${COMP_WORDS[1]}")"
     if [ -n "$matching_commands" ]; then
         COMPREPLY=( $matching_commands )
     fi
-
-    #echo "$COMP_LINE" >> ~/comp-test
-    #echo "$COMP_POINT" >> ~/comp-test
-    #echo "$COMP_CWORD" >> ~/comp-test
-    #echo "${COMP_WORDS[@]}" >> ~/comp-test
 }
 
-complete -F _p_completion p
+
+main() {
+    if which apt-get &>/dev/null; then
+        export P_COMPLETION_PKG_MANAGER='apt'
+    elif which brew &>/dev/null; then
+        export P_COMPLETION_PKG_MANAGER='brew'
+    elif which yum &>/dev/null; then
+        export P_COMPLETION_PKG_MANAGER='yum'
+    fi
+
+    complete -F _p_completion p
+}
 
 
-if [ $(which apt-get) ]; then
-    export P_COMPLETION_PKG_MANAGER='apt'
-elif [ $(which brew) ]; then
-    export P_COMPLETION_PKG_MANAGER='brew'
-elif [ $(which yum) ]; then
-    export P_COMPLETION_PKG_MANAGER='yum'
-fi
+main "$@"
